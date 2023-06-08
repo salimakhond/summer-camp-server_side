1992e6264af4dd118c2cb9b6560059c123585e8d
salimakhond <salimakhond@gmai.com>
2023-06-09 03:57:22 +0600

    user delete api done

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
