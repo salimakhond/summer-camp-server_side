b41bc4d5cebc4d91357612ffae5c8998a69e421d
salimakhond <salimakhond@gmai.com>
2023-06-09 01:37:56 +0600

    classes post api creacte done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -33,19 +33,21 @@ async function run() {
         const usersCollection = client.db("SummerCampDb").collection("users");
 
 
-        // users API
-        app.put('/users/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = req.body;
-            const query = { email: email }
-            const options = { upsert: true }
-            const updateDoc = {
-                $set: user
-            }
-            const result = await usersCollection.updateOne(query, updateDoc, options);
-            res.send(result)
-        })
 
+
+        // users API
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
 
 
         app.get('/users', async (req, res) => {
@@ -53,13 +55,18 @@ async function run() {
             res.send(result);
         })
 
-
-
         // classes API
         app.get('/classes', async (req, res) => {
             const result = await classesCollection.find().toArray();
             res.send(result)
         })
+
+        app.post('/classes', async (req, res) => {
+            const classes = req.body;
+            const result = await classesCollection.insertOne(classes);
+            res.send(result);
+        });
+
 
 
         // instructors API
