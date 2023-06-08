efa18369ed123822ba156b0f21dc1b8b91ce512c
salimakhond <salimakhond@gmai.com>
2023-06-09 02:29:36 +0600

    user instructor define done

--- index.js
+++ index.js
@@ -67,6 +67,25 @@ async function run() {
             res.send(result);
         })
 
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
         // classes API
         app.get('/classes', async (req, res) => {
             const result = await classesCollection.find().toArray();
