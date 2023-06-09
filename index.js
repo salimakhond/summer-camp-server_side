ed8b57ee10ed75611ea1fc20cdbf29f20dea31f5
salimakhond <salimakhond@gmai.com>
2023-06-09 16:01:34 +0600

    booking by email delete api done

--- index.js
+++ index.js
@@ -107,6 +107,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
